e018cb433a5f9ae3c4a5914dd784edaa09e448ec
xuyundong <xuyundong@douban.com>
2020-05-19 23:49:43 +0800

    Init

---
+++ MyES/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Author: bwangel<bwangel.me@gmail.com>
+# Date: 5,19,2020 23:46
+
+docker build -t bwangel/elasticsearch:7.6.2 .
---
+++ README.md
@@ -0,0 +1,3 @@
+## ES Cluster
+
+一个安装了分词插件的三节点 ElasticSearch 集群
